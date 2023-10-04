698938e2de2fa2149161ff3aa7f1b8840182bc27
pvantessel <132936837+pvantessel@users.noreply.github.com>
2023-10-04 15:12:32 +0200

    first-commit-les6-blog-part2

--- src/pages/newpostpage/NewPostPage.jsx
+++ src/pages/newpostpage/NewPostPage.jsx
@@ -1,105 +1,124 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewPostPage.css';
-import {useState} from "react";
 import FormInputField from "../../components/forminputfield/FormInputField.jsx";
 import calculateReadTime from "../../helpers/calculateReadTime.jsx";
-import {useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import axios from "axios";
 
 function NewPostPage() {
+    const [title, setTitle] = useState('');
+    const [subtitle, setSubtitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [content, setContent] = useState('');
+    const [sendForm, setSendForm] = useState(false);
+    const [id, setId] = useState(``);
 
-    const [title, setTitle] = useState("");
-    const [subtitle, setSubtitle] = useState("");
-    const [author, setAuthor] = useState("");
-    const [message, setMessage] = useState("");
-
-    const navigate = useNavigate();
-
-    function submitForm(e) {
+    async function submitForm(e) {
         e.preventDefault();
-        console.log(`Title: ${title}`);
-        console.log(`Subtitle: ${subtitle}`);
-        console.log(`Name: ${author}`);
-        console.log(`Message: ${message}`);
-        console.log(`Shares: 0`);
-        console.log(`Comments: 0`);
-        console.log(`Created: ${new Date().toISOString()}`);
-        console.log(`ReadTime: ${calculateReadTime(message)}`);
+        try {
+            const postData = {
+                title,
+                subtitle,
+                author,
+                content,
+                shares: 0,
+                comments: 0,
+                created: new Date().toISOString(),
+                readTime: calculateReadTime(content),
+            };
 
-        navigate('/overzicht');
+            const response = await axios.post('http://localhost:3000/posts', postData);
+            console.log(response);
+            setId(response.data.id);
+
+            if (response.status === 201) {
+                setSendForm(true);
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     return (
         <section>
             <div className="newpost-container">
-                <div className="newpost-message">
-                    <h3>Uw wilt een nieuwe Blog post plaatsen op BlOgventure, super leuk !! <br/>
-                        Vul hieronder het formulier in en uw post zal worden geplaatst. </h3>
-                </div>
 
-                <div className="messageContainer">
-                    <form onSubmit={submitForm} className="messageForm">
-
-                        <FormInputField
-                            labelInputClass="messageDivInput"
-                            labelName="inputTitle"
-                            labelText="Title"
-                            inputType="text"
-                            inputName="inputTitle"
-                            inputValue={title}
-                            setInput={setTitle}
-                            required={true}
-                        />
-
-                        <FormInputField
-                            labelInputClass="messageDivInput"
-                            labelName="inputSubtitle"
-                            labelText="Subtitle"
-                            inputType="text"
-                            inputName="inputSubtitle"
-                            inputValue={subtitle}
-                            setInput={setSubtitle}
-                            required={true}
-                        />
-
-                        <FormInputField
-                            labelInputClass="messageDivInput"
-                            labelName="inputAuteur"
-                            labelText="Name"
-                            inputType="text"
-                            inputName="inputAuteur"
-                            inputValue={author}
-                            setInput={setAuthor}
-                            required={true}
-                        />
-
-                        <div className="messageDivTextarea">
-                            <label htmlFor="message">
-                                Message
-                            </label>
-                            <textarea
-                                name="message"
-                                id="message"
-                                cols="50"
-                                rows="15"
-                                placeholder="Plaats hier uw blog message"
-                                value={message}
-                                minLength={300}
-                                maxLength={4000}
-                                required={true}
-                                onChange={(e) => setMessage(e.target.value)}
-                            >
-                        </textarea>
+                {!sendForm ?
+                    <>
+                        <div className="newpost-message">
+                            <h3>Uw wilt een nieuwe Blog post plaatsen op BlOgventure, super leuk !! <br/>
+                                Vul hieronder het formulier in en uw post zal worden geplaatst. </h3>
                         </div>
 
-                        <button type="submit">Post uw bericht</button>
-                    </form>
+                        <div className="messageContainer">
+                            <form onSubmit={submitForm} className="messageForm">
 
-                </div>
+                                <FormInputField
+                                    labelInputClass="messageDivInput"
+                                    labelName="inputTitle"
+                                    labelText="Title"
+                                    inputType="text"
+                                    inputName="inputTitle"
+                                    inputValue={title}
+                                    setInput={setTitle}
+                                    required={true}
+                                />
 
+                                <FormInputField
+                                    labelInputClass="messageDivInput"
+                                    labelName="inputSubtitle"
+                                    labelText="Subtitle"
+                                    inputType="text"
+                                    inputName="inputSubtitle"
+                                    inputValue={subtitle}
+                                    setInput={setSubtitle}
+                                    required={true}
+                                />
+
+                                <FormInputField
+                                    labelInputClass="messageDivInput"
+                                    labelName="inputAuteur"
+                                    labelText="Name"
+                                    inputType="text"
+                                    inputName="inputAuteur"
+                                    inputValue={author}
+                                    setInput={setAuthor}
+                                    required={true}
+                                />
+
+                                <div className="messageDivTextarea">
+                                    <label htmlFor="message">
+                                        Message
+                                    </label>
+                                    <textarea
+                                        name="message"
+                                        id="message"
+                                        cols="50"
+                                        rows="15"
+                                        placeholder="Plaats hier uw blog message"
+                                        value={content}
+                                        minLength={300}
+                                        maxLength={4000}
+                                        required={true}
+                                        onChange={(e) => setContent(e.target.value)}
+                                    >
+                        </textarea>
+                                </div>
+
+                                <button type="submit">Post uw bericht</button>
+                            </form>
+
+                        </div>
+
+                    </>
+                    :
+                    <div className="newpost-message">
+                        <h3>Yes, je post is toegevoegd !!! Je vindt hem <Link to={`/postdetails/${id}`}>hier</Link></h3>
+                    </div>
+                }
             </div>
         </section>
-
-
     );
 }
 
--- src/pages/overviewpage/OverviewPage.jsx
+++ src/pages/overviewpage/OverviewPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './OverviewPage.css';
-import blogItems from '../../constants/data.json';
 import {Link} from 'react-router-dom';
 import blogpost from "../../assets/blogpost.jpeg";
-
+import axios from "axios";
 
 function OverviewPage() {
+
+    const [displayPosts, setDisplayPosts] = useState([]);
+    const [error, setError] = useState(null);
+
+    async function getAllPosts() {
+        try {
+            const response = await axios.get('http://localhost:3000/posts');
+            setDisplayPosts(response.data);
+            setError(null);
+        } catch (e) {
+            console.error(e);
+            setError('Data ophalen is op dit moment even niet mogelijk, probeert u het later nog eens.');
+        }
+    }
+
     return (
         <section>
             <div className="overview-container">
@@ -14,18 +28,38 @@ function OverviewPage() {
 
                     <div className="blogpost-counter">
                         <p>Aantal posts op BlOgventure</p>
-                        <p className="counter-color">{blogItems.length}</p>
+                        <p className="counter-color">{displayPosts.length}</p>
                     </div>
                 </div>
 
-                <ul className="blog-card">
-                    {blogItems.map((blogItem) => {
-                        return <li className="blog-item-det" key={blogItem.id} >
-                                    <h4><Link to={`/postdetails/${blogItem.id}`}>{blogItem.title}</Link> ({blogItem.author})</h4>
-                                    <p>{blogItem.comments} reacties - {blogItem.shares} keer gedeeld</p>
-                              </li>
-                    })}
-                </ul>
+                <button type="button" onClick={getAllPosts}>Haal alle posts op</button>
+
+
+                {error ? (
+                    <p className="displayErrorOverviewPage">{error}</p>
+                ) : (
+                    <ul className="blog-card">
+                        {displayPosts.length > 0 ? (
+                            displayPosts.map((displayPost, index) => (
+                                <li className="blog-item-det" key={index}>
+                                    <h4>
+                                        <Link to={`/postdetails/${displayPost.id}`}>
+                                            {displayPost.title}
+                                        </Link>{' '}
+                                        ({displayPost.author})
+                                    </h4>
+                                    <p>
+                                        {displayPost.comments} reacties - {displayPost.shares} keer
+                                        gedeeld
+                                    </p>
+                                </li>
+                            ))
+                        ) : (
+                            <p className="displayNoPosts">Op dit moment zijn er nog geen posts beschikbaar.</p>
+                        )}
+                    </ul>
+                )}
+
             </div>
         </section>
     );
